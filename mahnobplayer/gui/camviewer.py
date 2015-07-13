1a785644c980131a039db4d022303f30e1cc8846
sfrullo <sfrulloo@gmail.com>
2015-07-13 12:36:07 +0200

    add media controll to camviewer

--- mahnobplayer/gui/camviewer.py
+++ mahnobplayer/gui/camviewer.py
@@ -9,6 +9,7 @@ from tkinter.filedialog import askopenfilenames
 
 from mahnobplayer.gui import basic
 from mahnobplayer.gui import videoframe
+from mahnobplayer.gui import mediacontrol
 
 class CamViewer(basic.BasicWindow):
     ''' CamViewer class implements the CamViewer GUI.
@@ -16,12 +17,46 @@ class CamViewer(basic.BasicWindow):
         videos at the same time with the chance of switch between them.
     '''
 
-
-    def __init__(self, parent, *args, **kwargs):
+    
+    def __init__(self, parent, controller, *args, **kwargs):
         ''' Init Function for the CamViewer class '''
         basic.BasicWindow.__init__(self, parent, *args, **kwargs)
         self.grid()
+        
+        self.__controller = controller
+        
+        self.createMenu()
+        
+        self.medialist = [(),(),()]
+        
+        self.videoframecontainer = basic.BasicFrame(self)
+        self.frames = dict(zip(range(6), [videoframe.selectableVideoFrame(self.videoframecontainer, self.medialist) for c in range(6)]))
+        index = [(0,0),(1,0),(2,0),
+                 (0,1),(1,1),(2,1)]
+        #configure column/row to get resizeble frames
+        for c,r in index:
+            self.rowconfigure(r, minsize=0, weight=1)
+            self.columnconfigure(c, minsize=0, weight=1)
 
+        color = ['white', 'red', 'green', 'blue', 'cyan', 'yellow']
+        for i, f in self.frames.items():
+            f.config(bg=color[i], padx=3, pady=3)
+            f.rowconfigure(0, weight=1)
+            f.columnconfigure(0, weight=1)
+            f.setVideoFrameHeight(200)
+            f.setVideoFrameWidth(200)
+            f.grid(column=index[i][0], row=index[i][1])
+        print(self.frames)
+        
+        self.videoframecontainer.grid(column=0,row=0, sticky='nesw')
+        
+        
+        self.mediacontrollecontainer = basic.BasicFrame(self)
+        self.mediacontroller = mediacontrol.MediaControl(self.mediacontrollecontainer, self.__controller)
+        self.mediacontrollecontainer.grid(column=0,row=1, sticky='nesw')
+        
+    
+    def createMenu(self):
         menudata = (
             [
                 "_File", (
@@ -29,14 +64,6 @@ class CamViewer(basic.BasicWindow):
                     ("_Add Video", "Control-o", self.on_add_video),
                     (), # Add a separator here
                     ("_Exit","Alt-F4", self.on_quit),
-                ),
-            ],
-            [
-                "_Edit", (
-                    ("_Undo", "Control-z", self.dummy),
-                    ("_Find", "F3", self.dummy),
-                    ("F_ooBar", "Control-Shift-A", self.dummy),
-                    ("Foo_Bar2","Alt-s", self.dummy),
                 ),
             ],
             [
@@ -52,35 +79,12 @@ class CamViewer(basic.BasicWindow):
         menu.addSubMenu(menudata)
         self.setMenu(menu)
         
-        
-        self.medialist = [(),(),()]
-        
-        
-        self.frames = dict(zip(range(6), [videoframe.selectableVideoFrame(self, self.medialist) for c in range(6)]))
-        index = [(0,0),(1,0),(2,0),
-                 (0,1),(1,1),(2,1)]
-        #configure column/row to get resizeble frames
-        for c,r in index:
-            self.rowconfigure(r, minsize=0, weight=1)
-            self.columnconfigure(c, minsize=0, weight=1)
-
-        color = ['white', 'red', 'green', 'blue', 'cyan', 'yellow']
-        for i, f in self.frames.items():
-            f.config(bg=color[i], padx=30, pady=30)
-            f.rowconfigure(0, weight=1)
-            f.columnconfigure(0, weight=1)
-            f.setVideoFrameHeight(200)
-            f.setVideoFrameWidth(200)
-            f.grid(column=index[i][0], row=index[i][1])
-        print(self.frames)
-        
     #-----------------------------------------------------------------------
     # Callbacks
     #-----------------------------------------------------------------------
     def on_quit(self):
         self.quit()
-
-
+        
     def on_add_video(self):
         fileSelected = askopenfilenames()
 
@@ -90,6 +94,6 @@ class CamViewer(basic.BasicWindow):
 
 if __name__ == '__main__':
     
-    app = CamViewer(tk.Tk())
+    app = CamViewer(tk.Tk(), None)
 
     app.mainloop()
