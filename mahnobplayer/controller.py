76caf63c31c4bae3c47a3db74edeffbcdd51af3a
sfrullo <sfrulloo@gmail.com>
2015-07-15 11:07:09 +0200

    add Player package

---
+++ mahnobplayer/controller.py
@@ -0,0 +1,21 @@
+'''
+Created on 14 Jul 2015
+
+@author: sfrullo
+'''
+
+class Controller:
+    ''' This class implements the main controller for the application '''
+
+
+    def __init__(self, ):
+        '''
+        Constructor
+        '''
+        pass
+    
+    def on_media_added(self, notifier, medialist):
+        print('work')
+        
+    def on_selection(self, notifier, videoframe, selection):
+        print('work on_selection')
--- mahnobplayer/main.py
+++ mahnobplayer/main.py
@@ -8,12 +8,18 @@ Created on 9 Jul 2015
 import tkinter as tk
 
 # local imports
-from mahnobplayer import gui
+from mahnobplayer.gui.camviewer import CamViewer
+from mahnobplayer.controller import Controller
 
 if __name__ == '__main__':
     # tk root hook
     root = tk.Tk()
     
+    ctr = Controller()
+    camview = CamViewer(root, controller=ctr)
+    
+    # set min size of the main window
+    root.update()
+    root.minsize(root.winfo_width(), root.winfo_height())
     # start root mainloop
     root.mainloop()
-    
